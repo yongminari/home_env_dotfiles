c2f21af368b231aa1c4af06b1a27497f3f60558a
yongminari <easyid21c@gmail.com>
2026-02-19 11:46:27 +0900

    modify neovim

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -6,9 +6,12 @@
     defaultEditor = true;
     viAlias = true; vimAlias = true;
 
-    # 1. 플러그인 목록 (설정 코드 제거하고 순수하게 목록만 유지)
+    # 1. 플러그인 목록
     plugins = with pkgs.vimPlugins; [
-      # UI 및 편의 기능
+      # [Theme] 가장 인기 있는 파스텔톤 테마
+      catppuccin-nvim
+
+      # [UI & Icons]
       vim-tmux-navigator 
       which-key-nvim 
       nvim-web-devicons
@@ -18,11 +21,11 @@
       plenary-nvim
       telescope-nvim
       
-      # 구문 강조 (에러 원인이었던 녀석)
+      # [Syntax]
       nvim-treesitter.withAllGrammars
     ];
 
-    # 2. Lua 설정 통합 (여기서 플러그인 설정을 안전하게 실행)
+    # 2. Lua 설정
     initLua = ''
       -- [기본 설정]
       vim.opt.number = true
@@ -32,33 +35,41 @@
       vim.opt.expandtab = true
       vim.g.mapleader = " "         
       vim.opt.clipboard = "unnamedplus"
+      vim.opt.termguicolors = true  -- 24비트 컬러 활성화 (테마 필수 설정)
 
-      -- [플러그인 설정]
-      -- pcall(protected call)로 감싸서, 플러그인 로드 실패 시에도 Neovim이 죽지 않게 함
-
-      -- 1. Lualine (상태바)
-      local status_ok, lualine = pcall(require, "lualine")
+      -- [테마 설정]
+      local status_ok, catppuccin = pcall(require, "catppuccin")
       if status_ok then
-        lualine.setup { options = { theme = 'auto' } }
+        catppuccin.setup({
+          flavour = "mocha", -- latte, frappe, macchiato, mocha
+          transparent_background = true, -- 배경 투명 (터미널 배경과 맞춤)
+        })
+        vim.cmd.colorscheme "catppuccin"
       end
 
-      -- 2. Neo-tree (파일 탐색기)
-      -- Ctrl+n 키맵핑
+      -- [플러그인 설정]
+      -- Lualine (상태바)
+      local status_ok, lualine = pcall(require, "lualine")
+      if status_ok then
+        lualine.setup { options = { theme = 'catppuccin' } }
+      end
+
+      -- Neo-tree (Ctrl+n으로 켜고 끄기)
       vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', { silent = true })
 
-      -- 3. Telescope (파일 찾기)
+      -- Telescope (파일 찾기)
       local status_ok, telescope_builtin = pcall(require, "telescope.builtin")
       if status_ok then
         vim.keymap.set('n', '<leader>f', telescope_builtin.find_files, {})
         vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, {})
       end
 
-      -- 4. Treesitter (구문 강조) - 에러 수정 핵심!
+      -- Treesitter
       local status_ok, configs = pcall(require, "nvim-treesitter.configs")
       if status_ok then
         configs.setup {
           highlight = { enable = true },
-          indent = { enable = true },  -- 들여쓰기도 개선됨
+          indent = { enable = true },
         }
       end
     '';

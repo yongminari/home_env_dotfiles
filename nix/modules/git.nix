73b8e94d5b7feeea191577c707cc252ebf4221ee
yongminari <easyid21c@gmail.com>
2026-03-03 16:21:01 +0900

    update git info

--- nix/modules/git.nix
+++ nix/modules/git.nix
@@ -3,14 +3,13 @@
 {
   programs.git = {
     enable = true;
-    # 'settings'가 아니라 아래와 같이 직접 지정해야 합니다.
-    userName = "yongminari";
-    userEmail = "easyid21c@gmail.com";
-
-    # 나머지 설정들 (init, core 등)은 여기에 넣습니다.
-    extraConfig = {
+    settings = {
+      user = {
+        name = "yongminari";
+        email = "easyid21c@gmail.com";
+      };
       init.defaultBranch = "main";
-      core.ignorecase = true; # 아까 논의한 윈도우-리눅스 혼용 설정
+      core.ignorecase = true;
     };
   };
 }

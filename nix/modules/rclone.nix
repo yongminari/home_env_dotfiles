1d5af7c13ddb5da102990a7d048a71735fdd2f52
yongminari <easyid21c@gmail.com>
2026-03-08 16:48:34 +0900

    add cloud storages(gdrvie, onedrive)

--- nix/modules/rclone.nix
+++ nix/modules/rclone.nix
@@ -5,21 +5,15 @@
   systemd.user.services = {
     rclone-mount-gdrive = {
       Unit = {
-        Description = "rclone mount for Google Drive";
+        Description = "rclone-mount-gdrive";
         After = [ "network-online.target" ];
         Wants = [ "network-online.target" ];
       };
       Service = {
         Type = "simple";
-        # %h는 홈 디렉토리를 의미합니다.
         ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/mnt/gdrive";
-        ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: %h/mnt/gdrive \
-          --vfs-cache-mode full \
-          --vfs-cache-max-age 24h \
-          --buffer-size 128M \
-          --vfs-read-chunk-size 64M \
-          --vfs-read-chunk-size-limit 1G";
-        ExecStop = "/bin/fusermount -u %h/mnt/gdrive";
+        ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: %h/mnt/gdrive --vfs-cache-mode full --vfs-cache-max-age 24h --buffer-size 128M --vfs-read-chunk-size 64M --vfs-read-chunk-size-limit 1G";
+        ExecStop = "/usr/bin/fusermount3 -u %h/mnt/gdrive";
         Restart = "on-failure";
         RestartSec = "10s";
       };
@@ -30,20 +24,15 @@
 
     rclone-mount-onedrive = {
       Unit = {
-        Description = "rclone mount for OneDrive";
+        Description = "rclone-mount-onedrive";
         After = [ "network-online.target" ];
         Wants = [ "network-online.target" ];
       };
       Service = {
         Type = "simple";
         ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/mnt/onedrive";
-        ExecStart = "${pkgs.rclone}/bin/rclone mount onedrive: %h/mnt/onedrive \
-          --vfs-cache-mode full \
-          --vfs-cache-max-age 24h \
-          --buffer-size 128M \
-          --vfs-read-chunk-size 64M \
-          --vfs-read-chunk-size-limit 1G";
-        ExecStop = "/bin/fusermount -u %h/mnt/onedrive";
+        ExecStart = "${pkgs.rclone}/bin/rclone mount onedrive: %h/mnt/onedrive --vfs-cache-mode full --vfs-cache-max-age 24h --buffer-size 128M --vfs-read-chunk-size 64M --vfs-read-chunk-size-limit 1G";
+        ExecStop = "/usr/bin/fusermount3 -u %h/mnt/onedrive";
         Restart = "on-failure";
         RestartSec = "10s";
       };

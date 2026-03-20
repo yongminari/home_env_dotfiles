df877053e47286df100fb801f4e9b706b72bc227
yongminari <easyid21c@gmail.com>
2026-03-20 22:21:17 +0900

    removed unnecessary alias

--- nix/modules/zsh.nix
+++ nix/modules/zsh.nix
@@ -66,7 +66,6 @@
 
     # (특수 에일리어스는 유지, 공통 에일리어스는 home.nix에서 로드됨)
     shellAliases = {
-      hypr_shortcuts = ''printf "\n  \033[1;34m🪟 Hyprland & Wofi Shortcuts\033[0m\n\n  \033[1;32mSuper + Enter\033[0m       Launch Ghostty\n  \033[1;32mSuper + R\033[0m           Launch Wofi (Runner)\n  \033[1;32mSuper + W\033[0m           Work Automation (Slack/Git/Gmail)\n  \033[1;32mSuper + F\033[0m           Toggle Fullscreen\n  \033[1;32mSuper + hjkl\033[0m        Move Focus (Window/Monitor)\n  \033[1;32mSuper + , / .\033[0m       Cycle Monitor Focus\n  \033[1;32mSuper + Shift + hjkl\033[0m Move Window Position\n  \033[1;32mSuper + Alt + hjkl\033[0m   Resize Active Window\n  \033[1;32mSuper + S / Shift+S\033[0m  Scratchpad Toggle / Move window\n  \033[1;32mSuper + Space\033[0m       IBus Language Toggle\n  \033[1;32mSuper + Q\033[0m           Kill Active Window\n  \033[1;32mSuper + M\033[0m           Exit Hyprland (Logout)\n  \033[1;32mSuper + V\033[0m           Toggle Floating\n  \033[1;32mSuper + 1~0\033[0m         Switch Workspace (1-10)\n  \033[1;32mSuper + Shift + 1~0\033[0m Move to Workspace\n  \033[1;32mSuper + Scroll\033[0m      Switch Workspaces\n  \033[1;32mSuper + Mouse L/R\033[0m   Drag to Move / Resize Window\n\n" | lolcat'';
     };
   };
 }

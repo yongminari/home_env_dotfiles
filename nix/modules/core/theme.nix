ccedb94435c4edf0efe31eb056c9177e7902aeb5
yongminari <easyid21c@gmail.com>
2026-05-06 10:41:13 +0900

    update

--- nix/modules/core/theme.nix
+++ nix/modules/core/theme.nix
@@ -35,13 +35,29 @@
 
   home.packages = with pkgs; [
     catppuccin-kvantum
+    papirus-icon-theme
+    adwaita-icon-theme
+    hicolor-icon-theme
   ];
 
   # Qt 앱들을 GTK 테마와 동기화
   qt = {
     enable = true;
     platformTheme.name = "gtk";
-    style.name = "adwaita-dark";
+    style.name = "kvantum";
+  };
+
+  # dconf 활성화
+  dconf.enable = true;
+
+  # dconf 설정으로 GNOME 환경의 아이콘 및 커서 테마 지정
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      cursor-size = 52;
+      cursor-theme = "Bibata-Modern-Ice";
+      icon-theme = "Papirus-Dark";
+      gtk-theme = "Ayu-Dark";
+    };
   };
 
   # 커서 테마를 X11과 Wayland 모두에 적용
@@ -52,4 +68,10 @@
     name = "Bibata-Modern-Ice";
     size = 52;
   };
+
+  # 커서 테마 강제 연결 (Legacy 및 XWayland 호환성)
+  home.file.".icons/default/index.theme".text = ''
+    [icon theme]
+    Inherits=Bibata-Modern-Ice
+  '';
 }
